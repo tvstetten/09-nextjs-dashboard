e879d09210a23f72f5053828c135f894a34b36bd
Thomas von Stetten <tvstetten@tvs-soft.de>
2023-11-17 13:33:07 +0100

    Missing "ALT" property in invoice table-images

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -28,6 +28,7 @@ export default async function InvoicesTable({
                     <div className="mb-2 flex items-center">
                       <Image
                         src={invoice.image_url}
+                        alt={`${invoice.name}'s profile picture`}
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
@@ -86,6 +87,7 @@ export default async function InvoicesTable({
                     <div className="flex items-center gap-3">
                       <Image
                         src={invoice.image_url}
+                        alt={`${invoice.name}'s profile picture`}
                         className="rounded-full"
                         width={28}
                         height={28}
