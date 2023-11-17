99c59ca07bad85394c8cba09cbf8807ee468e024
Thomas von Stetten <tvstetten@tvs-soft.de>
2023-11-17 13:17:28 +0100

    Before Database Chapter

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter, Lusitana, Merriweather } from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin']
+});
+export const lusitana = Lusitana({
+    weight: ["400", "700"],
+    subsets: ['latin'],
+});
+
+export const secondFont = Merriweather({
+    weight: ["400", "700"],
+    subsets: ['latin'],
+});
