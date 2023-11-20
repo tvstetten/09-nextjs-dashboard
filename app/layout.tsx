9203e38c3cf03091410bac91b46a5543c50987db
Thomas von Stetten <tvstetten@tvs-soft.de>
2023-11-20 09:57:50 +0100

    Chapter 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next';
 
+// Either in the layout or the page
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Acme Dashboard',
+        default: 'Acme Dashboard',
+    },
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function RootLayout({
     children,
 }: {
--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -4,6 +4,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -36,7 +37,6 @@ export async function createInvoice(prevState: State, formData: FormData) {
     amount: formData.get("amount"),
     status: formData.get("status")
   })
-  console.log("🚀 ~ file: actions.tsx:40 ~ createInvoice ~ validatedFields:", validatedFields)
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
@@ -96,5 +96,20 @@ export async function deleteInvoice(id: string) {
     return { message: 'Deleted Invoice.' };
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice' };
+  }
+}
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
   }
 }
